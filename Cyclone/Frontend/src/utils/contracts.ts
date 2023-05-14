abcbf1b4ba072a108a248e56b7ba88ce62973e3b
yagnadeepxo <77982741+yagnadeepxo@users.noreply.github.com>
2023-05-14 12:32:42 +0530

    updated contract address

--- Cyclone/Frontend/src/utils/contracts.ts
+++ Cyclone/Frontend/src/utils/contracts.ts
@@ -1,5 +1,5 @@
 import { ethers } from "ethers";
 import tornadoJSON from "../json/Tornado.json";
 
-export const tornadoAddress = "0x552568427Beeb742f62175F0c337C48C6134b9a0"
+export const tornadoAddress = "0x4663086ed4A490Ea0f3bb1787E38AFFC417c5397"
 export const tornadoInterface = new ethers.utils.Interface(tornadoJSON);
